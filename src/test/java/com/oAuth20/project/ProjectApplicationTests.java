2e9b0ca772132d4ab00afa66bbcc268a80981b72
abineshgovindan <abineshgovindan1@gmail.com>
2024-04-27 16:07:22 +0530

    v1

---
+++ src/test/java/com/oAuth20/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oAuth20.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
